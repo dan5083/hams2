1d42ecb81412c62cc563e0233e4cd9a8a26c1b8b
dan5083 <dan5083@outlook.com>
2025-08-20 15:58:04 +0100

    Redesign PPI form with treatment buttons and dynamic criteria sections

--- config/initializers/operation_library.rb
+++ config/initializers/operation_library.rb
@@ -1,2 +1,5 @@
 # config/initializers/operation_library.rb
 require Rails.root.join('app', 'operation_library', 'operation')
+require Rails.root.join('app', 'operation_library', 'anodising', 'standard_anodising')
+require Rails.root.join('app', 'operation_library', 'anodising', 'hard_anodising')
+require Rails.root.join('app', 'operation_library', 'anodising', 'chromic_anodising')
