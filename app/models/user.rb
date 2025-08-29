3b504ff325d8c9032067921fc387d2f1a0f21455
dan5083 <dan5083@outlook.com>
2025-08-29 11:00:53 +0100

    Add magic link methods to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,6 +30,39 @@ class User < ApplicationRecord
     enabled
   end
 
+  # Magic link token generation
+  def generate_magic_link_token
+    self.magic_link_token = SecureRandom.urlsafe_base64(32)
+    self.magic_link_expires_at = 15.minutes.from_now
+    save!
+    magic_link_token
+  end
+
+  # Check if magic link is valid
+  def magic_link_valid?(token)
+    return false unless magic_link_token.present? && magic_link_expires_at.present?
+    return false if magic_link_expires_at < Time.current
+
+    ActiveSupport::SecurityUtils.secure_compare(magic_link_token, token)
+  end
+
+  # Clear magic link after use
+  def clear_magic_link!
+    self.magic_link_token = nil
+    self.magic_link_expires_at = nil
+    save!
+  end
+
+  # Class method to find by valid magic link
+  def self.find_by_magic_link(token)
+    return nil if token.blank?
+
+    user = find_by(magic_link_token: token)
+    return nil unless user&.magic_link_valid?(token)
+
+    user
+  end
+
   private
 
   def set_defaults
