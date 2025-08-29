38cc00612720a55f5d683e0c90a46f70973b3cca
dan5083 <dan5083@outlook.com>
2025-08-29 11:06:37 +0100

    Add magic link email mailer and templates

--- app/mailers/passwords_mailer.rb
+++ app/mailers/passwords_mailer.rb
@@ -3,4 +3,10 @@ class PasswordsMailer < ApplicationMailer
     @user = user
     mail subject: "Reset your password", to: user.email_address
   end
+
+  def magic_link(user, token)
+    @user = user
+    @magic_link_url = magic_link_url(token)
+    mail subject: "Your login link for HAMS", to: user.email_address
+  end
 end
