4e8794852a8ff9a5cd09e973ed27ac477721e341
dan5083 <dan5083@outlook.com>
2025-09-24 15:43:36 +0100

    vxvx

--- app/controllers/external_ncrs_controller.rb
+++ app/controllers/external_ncrs_controller.rb
@@ -226,11 +226,7 @@ end
           render_to_string(
             template: 'external_ncrs/response',
             layout: false
-          ),
-          format: 'A4',
-          margin: { top: '1cm', bottom: '1cm', left: '1cm', right: '1cm' },
-          print_background: true,
-          prefer_css_page_size: true
+          )
         ).to_pdf
 
         send_data pdf,
