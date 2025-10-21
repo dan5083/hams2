08a087b5d82e2c936bd0c1551ed4402c0c05e9b1
dan5083 <dan5083@outlook.com>
2025-10-21 13:33:39 +0100

    I

--- app/mailers/order_acknowledgement_mailer.rb
+++ app/mailers/order_acknowledgement_mailer.rb
@@ -2,15 +2,15 @@
 class OrderAcknowledgementMailer < ApplicationMailer
   def order_confirmation(customer_order, works_orders)
     @customer_order = customer_order
-    @works_orders = works_orders.includes(:part, :release_level, :transport_method)
+    @works_orders = works_orders  # Remove .includes() - these are already loaded objects
     @customer = customer_order.customer
 
     # Calculate totals
-    @total_quantity = @works_orders.sum(:quantity)
-    @total_value = @works_orders.sum(:lot_price)
+    @total_quantity = @works_orders.sum(&:quantity)
+    @total_value = @works_orders.sum(&:lot_price)
 
     # Get unique customer references if any
-    @customer_references = @works_orders.pluck(:customer_reference).compact.uniq.reject(&:blank?)
+    @customer_references = @works_orders.map(&:customer_reference).compact.uniq.reject(&:blank?)
 
     mail(
       to: @customer.contact_email,
