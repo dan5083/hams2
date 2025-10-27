119cef9ff2db4178a3bd91e0406c79bfd2d28777
dan5083 <dan5083@outlook.com>
2025-10-27 13:06:10 +0000

    X

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,15 +1,51 @@
 # app/controllers/organizations_controller.rb
 class OrganizationsController < ApplicationController
   def index
-    @customers = Organization.customers
-                            .enabled
-                            .includes(:buyers, :xero_contact)
-                            .order(:name)
+    # Start with base query
+    @organizations = Organization.includes(:buyers, :xero_contact)
 
-    @suppliers = Organization.suppliers
-                            .enabled
-                            .includes(:xero_contact)
-                            .order(:name)
+    # Filter by type (customers/suppliers)
+    case params[:type]
+    when 'customers'
+      @organizations = @organizations.customers
+    when 'suppliers'
+      @organizations = @organizations.suppliers
+    else
+      # Show all by default
+      @organizations = @organizations
+    end
+
+    # Filter by enabled status
+    case params[:status]
+    when 'enabled'
+      @organizations = @organizations.enabled
+    when 'disabled'
+      @organizations = @organizations.disabled
+    else
+      # Show enabled by default
+      @organizations = @organizations.enabled
+    end
+
+    # Filter by buyer presence
+    if params[:buyers].present?
+      case params[:buyers]
+      when 'with_buyers'
+        @organizations = @organizations.joins(:buyers).distinct
+      when 'without_buyers'
+        @organizations = @organizations.left_joins(:buyers)
+                                      .where(buyers: { id: nil })
+      end
+    end
+
+    # Search by organization name
+    if params[:search].present?
+      @organizations = @organizations.where("name ILIKE ?", "%#{params[:search]}%")
+    end
+
+    # Order and paginate
+    @organizations = @organizations.order(:name)
+                                  .page(params[:page])
+                                  .per(20)
   end
 
   def sync_from_xero
