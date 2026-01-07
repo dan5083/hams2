ef3dd8ca95c5c82db48324f024f7b2504b450a52
Dan Bayliss <dan5083@outlook.com>
2026-01-07 16:09:17 +0000

    Complete specifications feature with bulk import

---
+++ specs_export.sql
@@ -0,0 +1,14 @@
+-- Specifications export for Heroku
+-- Generated: 2026-01-07 14:07:22 +0000
+-- Total records: 255
+
+INSERT INTO specifications (id, title, description, version, is_qs, archived, document_data, created_by_id, updated_by_id, created_at, updated_at) VALUES (
+  '7cd174c6-5089-4ecc-ab4e-a7c0c764d797',
+  '31.10 3-5-16 Styles Prec',
+  NULL,
+  NULL,
+  true,
+  false,
+  '{"content_type":"application/pdf","cloudinary_url":"https://res.cloudinary.com/deydspzts/raw/upload/v1767783709/Specs/QS/2026/31-10-3-5-16-styles-prec_20260107_110148_31_10_3-5-16_Styles_Prec.pdf","file_size_bytes":53976,"original_filename":"31.10 3-5-16 Styles Prec.pdf","cloudinary_public_id":"Specs/QS/2026/31-10-3-5-16-styles-prec_20260107_110148_31_10_3-5-16_Styles_Prec.pdf","document_uploaded_at":"2026-01-07T11:01:50Z"}'::jsonb,
+  'c796de6e-2163-4b49-9b90-24f18b484949',
+  NULL,
