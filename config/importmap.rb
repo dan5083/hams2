f8ec498c0b15f1a70d2c71cef745cb9eca87bc2e
Dan Bayliss <dan5083@outlook.com>
2026-02-03 13:32:06 +0000

    XI

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,5 +6,5 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
 # Trix Editor
-pin "trix"
-pin "@rails/actiontext", to: "actiontext.js"
+pin "trix" # @2.1.16
+pin "@rails/actiontext", to: "@rails--actiontext.js" # @8.1.200
