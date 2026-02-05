c6c9401085b0fe70da3d290ab9f5b892f6b1fc83
Dan Bayliss <dan5083@outlook.com>
2026-02-05 08:04:09 +0000

    XV

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,6 +5,4 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-# Trix Editor
-pin "trix" # @2.1.16
-pin "@rails/actiontext", to: "@rails--actiontext.js" # @8.1.200
+# Quill is loaded via CDN in the layout, no need to pin here
